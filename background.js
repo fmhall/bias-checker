84cea50dc05cb808732408067e3c851e6aa3271a
Melissa Masia <melissamasia@gmail.com>
2018-11-03 01:25:51 -0500

    Fmh firebase (#4)
    added firebase

---
+++ background.js
@@ -0,0 +1,8 @@
+// listen for any changes to the URL of any tab.
+console.log("background");
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+    // if (tab.url.toLowerCase().indexOf("https://www.nytimes.com") > -1){
+    chrome.pageAction.show(tab.id);
+    chrome.tabs.executeScript(null, {"file": "log.js"});
+    // }
+});
