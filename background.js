7be24b3897a55da5c16043146359ba7cf1c64e40
Mason Hall <masonhall@gmail.com>
2018-11-03 01:50:25 -0500

    log

--- background.js
+++ background.js
@@ -3,6 +3,6 @@ console.log("background");
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
     // if (tab.url.toLowerCase().indexOf("https://www.nytimes.com") > -1){
     chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(null, {"file": "log.js"});
+    chrome.tabs.executeScript(null, {"file": "popup.js"});
     // }
 });
